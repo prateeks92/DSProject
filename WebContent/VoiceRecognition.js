676dca2f1c354527ec97e5fb992a75cc29fd7bb9
Prem Ankur <premankur19@gmail.com>
2016-11-11 16:03:55 -0500

    Added interfacing to the api.ai, JSON response from api.ai is being received. Parsing the JSON data and TTS is Pending for the response.

--- WebContent/VoiceRecognition.js
+++ WebContent/VoiceRecognition.js
@@ -18,6 +18,7 @@
         document.getElementById('transcript').innerHTML
                                  = e.results[0][0].transcript;
         recognition.stop();
+        send();
 //        document.getElementById('labnol').submit();
       };
       
@@ -25,4 +26,52 @@
         recognition.stop();
       }
     }
+    send();
   }
+ 
+ var baseUrl = "https://api.api.ai/v1/";
+ var accessToken = "35b35640bfc745819840070409a7059b";
+ function send() {
+		var text =  document.getElementById('transcript').innerHTML;
+		$.ajax({
+			type: "POST",
+			url: baseUrl + "query?v=20150910",
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			headers: {
+				"Authorization": "Bearer " + accessToken
+			},
+			data: JSON.stringify({ q: text, lang: "en" }),
+			success: function(data) {
+				setResponse(JSON.stringify(data, undefined, 2));
+			},
+			error: function() {
+				setResponse("Internal Server Error");
+			}
+		});
+		setResponse("Loading...");
+	}
+ function switchRecognition() {
+		if (recognition) {
+			stopRecognition();
+		} else {
+			startAsr();
+		}
+	}
+	function setInput(text) {
+		$("#transcript").val(text);
+		send();
+	}
+	function updateRec() {
+		$("#rec").text(recognition ? "Stop" : "Speak");
+	}
+	function setResponse(val) {
+		document.getElementById('reply').innerHTML = val;
+	}
+	function stopRecognition() {
+		if (recognition) {
+			recognition.stop();
+			recognition = null;
+		}
+		updateRec();
+	}
