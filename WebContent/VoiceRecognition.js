cf56a09e8ad8acdd82316a4ed8a1c036ff3aad7c
Prem Ankur <premankur19@gmail.com>
2016-11-13 13:48:17 -0500

    Removed bugs. TechAssist Version 1.2

--- WebContent/VoiceRecognition.js
+++ WebContent/VoiceRecognition.js
@@ -153,7 +153,7 @@ function tts()
     u.voiceURI = 'native';
    	u.volume = 1; // 0 to 1
     u.rate = 1; // 0.1 to 10
-   	u.pitch = 2; //0 to 2
+   	u.pitch = 0.1; //0 to 2
     u.text = reply;
    	u.lang = 'en-US';
     u.rate = 1;
